764b98a9e1a7d2ba684b375f1c1ff5ec51da20b1
tiagosokulski <tiagosokulski@gmail.com>
2026-01-08 20:25:08 -0300

    Refactor schema definition using defineField

--- schemaTypes/solucao.ts
+++ schemaTypes/solucao.ts
@@ -1,14 +1,16 @@
-export default {
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
   name: 'solucao',
   title: 'Solução',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'setor',
       title: 'Setor',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
@@ -16,27 +18,57 @@ export default {
         source: 'setor',
         maxLength: 96,
       },
-    },
-    {
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'category' } }],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
       name: 'pitch',
       title: 'Pitch de Vendas',
-      type: 'text',
-    },
-    {
-      name: 'explicacao',
+      type: 'string',
+    }),
+    defineField({
+      name: 'explicacaoDetalhada',
       title: 'Explicação Detalhada',
-      type: 'array',
-      of: [{ type: 'block' }],
-    },
-    {
-      name: 'link',
+      type: 'blockContent',
+    }),
+    defineField({
+      name: 'linkTeste',
       title: 'Link para Testar',
       type: 'url',
-    },
-    {
+    }),
+    defineField({
       name: 'ordem',
       title: 'Ordem',
       type: 'number',
-    },
+    }),
   ],
-};
+
+  preview: {
+    select: {
+      title: 'setor',
+      media: 'mainImage',
+      subtitle: 'pitch',
+    },
+    prepare(selection) {
+      const { pitch } = selection
+      return { ...selection, subtitle: pitch && `${pitch}` }
+    },
+  },
+})
